ccc593a5b1de320b64800bc2b6cb1bfb2bada0b1
Ida Darmochwał <ida.mdart@gmail.com>
2021-10-06 15:34:06 +0200

    Enable tasks modification

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,129 @@
 import React from 'react';
+import { io } from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-        Learn React
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    tasks: [],
+    taskName: '',
+  }
+
+  componentDidMount(){
+    this.socket = io((process.env.NODE_ENV === 'production') ? '' : 'localhost:8000', { transports: ["websocket"] });
+
+    this.socket.on('updateData', (tasks) => this.updateTasks(tasks));
+    this.socket.on('addTask', (task) => this.addTask(task));
+    this.socket.on('editTask', (task) => this.editTask(task));
+    this.socket.on('removeTask', (index) => this.removeTask(index));
+  }
+
+  updateTasks(newData) {
+    this.setState({
+      tasks: newData
+    })
+  }
+
+  addTask(task) {
+    const { tasks } = this.state;
+
+    tasks.push(task);
+    this.setState({
+      tasks: tasks
+    })
+  }
+
+  removeTask = (index, isLocal) => {
+    const { tasks } = this.state;
+
+    let taskIndex = tasks.findIndex(task => task === index);
+    if(taskIndex && isLocal){
+      tasks.splice(index, 1);
+      this.setState({
+        tasks: tasks
+      })
+      this.socket.emit('removeTask', {index});
+    }
+  }
+
+  editTask = (task) => {
+    const { tasks } = this.state;
+
+    const newName = prompt('Edit task\'s name');
+    let index = tasks.findIndex(el => el.name === task);
+    tasks[index].name = newName;
+
+    this.setState({
+      tasks: tasks
+    })
+    this.socket.emit('editTask', tasks[index]);
+  }
+
+  submitForm = (e) => {
+    const { taskName } = this.state;
+    e.preventDefault();
+
+    if(taskName === '') {
+      alert('Please enter task\'s name');
+    } else {
+      let id = uuidv4();
+      const taskData = {
+        id: id,
+        name: taskName
+      }
+
+      this.socket.emit('addTask', taskData);
+      this.addTask(taskData);
+      this.setState({
+        taskName: ''
+      })
+    }
+  }
+  
+  render() {
+
+    const { removeTask, submitForm } = this;
+    const { tasks, taskName } = this.state;
+
+    return (
+      <div className="App">
+        <header>
+          <h1>To-Do App</h1>
+        </header>
+  
+        <section id="tasks-section" className="tasks-section">
+          <h2>Tasks</h2>
+  
+          <ul id="tasks-list" className="tasks-section__list">
+            {tasks && tasks.map((task, index) => (
+              <li className="task" key={index}>
+                <p>{task.name}</p>
+                <button className='btn' onClick={() => this.editTask(task.name)}>Edit</button>
+                <button className="btn btn-red" onClick={() => removeTask(index, true)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+
+          <form id="add-task-form">
+            <input 
+              autoComplete="off" 
+              id="task-content" 
+              className="text-input" 
+              type="text" 
+              placeholder="Type task's description"
+              value={taskName}
+              onChange={e => (
+                this.setState({
+                  taskName: e.currentTarget.value
+                })
+              )} 
+            />
+            <button className="btn" type="submit" onClick={submitForm}>Add</button>
+          </form>
+
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
-const { env } = require('process');
 const socket = require('socket.io');
 
 const app = express();
+
+const tasks = [];
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running');
@@ -13,4 +14,43 @@ const io = socket(server);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+// app.use(express.static(path.join(__dirname, '/client')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
+// });
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+
+  io.to(socket.id).emit('updateData', tasks);
+
+  socket.on('addTask', ({id, name}) => {
+    console.log(`Task added! Its name: ${name}`);
+
+    socket.broadcast.emit('addTask', {id, name});
+    tasks.push({id, name});
+  });
+
+  socket.on('editTask', ({id, name}) => {
+    const index = tasks.findIndex(task => task.id === id);
+    tasks[index].name = name;
+    socket.broadcast.emit('editTask', {id, name});
+    console.log(`Task edited! Its new name: ${name}`);
+  })
+
+  socket.on('removeTask', (index) => {
+    console.log(`Task removed! Its index: ${index}`);
+
+    tasks.splice(index, 1);
+    socket.broadcast.emit('removeTask', index);
+  });
+
+  socket.on('disconnect', () => { 
+    console.log('Oh, socket ' + socket.id + ' has left');
+  });
+
+  console.log('I\'ve added a listener on tasks and disconnect events \n');
 });
